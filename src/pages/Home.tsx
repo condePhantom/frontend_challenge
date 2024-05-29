25db20c5c0a87ad3530b108bea344bcd17d76d0b
Cesar Perez <ing.cesarperz@gmail.com>
2024-05-29 16:46:49 -0700

    feat: add hero banner and movie card list components to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,45 @@
-import {FC} from 'react';
+import React, { useEffect, useState, FC } from "react";
+import { Container, Typography } from "@mui/material";
+import EmptyLayout from "../components/layout/Empty";
+import HeroBanner from "../components/molecules/HeroBanner";
+import MovieCardList from "../components/molecules/MovieCardList";
+import { Movie } from "../types/Movie";
+import { getPopularMovies } from "../services/movies";
 
 const Home: FC = () => {
-  
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [bannerMovie, setBannerMovie] = useState<Movie>({
+    id: 0,
+    title: "",
+    poster_path: "",
+    overview: "",
+    vote_average: 0,
+    genre_ids: [],
+  });
+
+  useEffect(() => {
+    getPopularMovies().then((data) => {
+      if (data) {
+        setMovies(data);
+        setBannerMovie(data[0]);
+      }
+    });
+  }, []);
+  console.log(movies);
 
   return (
-    
-      <div >
-        
-      <h1>Home</h1>
-      
-      </div>
-    
+    <div>
+      <EmptyLayout>
+        <Container sx={{ py: 0, mt: 8 }}>
+          <HeroBanner movie={bannerMovie} />
+          <Typography variant="h3" component="h2" gutterBottom mt={4}>
+            Popular Movies
+          </Typography>
+          <MovieCardList movies={movies} />
+        </Container>
+      </EmptyLayout>
+    </div>
   );
-}
+};
 
 export default Home;
