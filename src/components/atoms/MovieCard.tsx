027e9a27baff9b22d141ccf1d5a834e740c7f8f3
Cesar Perez <ing.cesarperz@gmail.com>
2024-05-29 16:44:53 -0700

    feat: add movie card component

---
+++ src/components/atoms/MovieCard.tsx
@@ -0,0 +1,46 @@
+import { FC } from "react";
+import { Grid, Card, CardMedia, CardContent, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Movie } from "../../types/Movie";
+
+const image_url = import.meta.env.VITE_IMAGE_URL;
+
+interface MovieCardProps {
+  movie: Movie;
+}
+
+const MovieCard: FC<MovieCardProps> = ({ movie }) => {
+  console.log(movie);
+  return (
+    <Grid
+      item
+      key={movie.id}
+      xs={12}
+      sm={6}
+      md={4}
+      sx={{ height: "100%", padding: 0, mb: 3 }}
+    >
+      <Card
+        sx={{ backgroundColor: "rgba(64,11,7, 0.6)", borderRadius: 6 }}
+        elevation={2}
+      >
+        <Link to={`/movie/${movie.id}`} style={{ textDecoration: "none" }}>
+          <CardMedia
+            component="img"
+            height="330"
+            image={`${image_url}/w300/${movie.poster_path}`}
+            alt={movie.title}
+            sx={{ objectFit: "contain" }}
+          />
+        </Link>
+        <CardContent sx={{ p: 1 }}>
+          <Typography variant="h6" component="p" color="white">
+            {movie.title}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default MovieCard;
