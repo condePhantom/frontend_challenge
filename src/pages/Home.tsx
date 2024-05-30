808c6e42d71d8c5bbd0e174fa15a7e3eb20afc42
Cesar Perez <ing.cesarperz@gmail.com>
2024-05-30 13:12:40 -0700

    feat: integrate movieCardListContainer component in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,78 +1,52 @@
-import React, { useEffect, useState, FC } from "react";
-import { Container, Box, Typography, Pagination } from "@mui/material";
+import React, { useEffect, FC } from "react";
+import { Container, Skeleton, Grid } from "@mui/material";
 import EmptyLayout from "../components/layout/Empty";
 import HeroBanner from "../components/molecules/HeroBanner";
-import MovieCardList from "../components/molecules/MovieCardList";
-import { Movie } from "../types/Movie";
-import { getPopularMovies } from "../services/movies";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../store";
+import { fetchPopularMovies } from "../store/movieSlice";
+import MovieCardListContainer from "../components/organisms/MovieCardListContainer";
 
 const Home: FC = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [bannerMovie, setBannerMovie] = useState<Movie>({
-    id: 0,
-    title: "",
-    poster_path: "",
-    overview: "",
-    vote_average: 0,
-    genre_ids: [],
-  });
-  const [page, setPage] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(1);
+  const dispatch: AppDispatch = useDispatch();
+  const { movies, status } = useSelector((state: RootState) => state.movies);
+
+  console.log(movies);
 
   useEffect(() => {
-    getPopularMovies(page).then((data) => {
-      if (data) {
-        setMovies(data.results);
-        setBannerMovie(data.results[0]);
-        //NOTE: should use data.total_pages but TMBD said that petition only works with pages from 1 to 500
-        setTotalPages(500);
-      }
-    });
-  }, [page]);
+    //First call is with page 1
+    dispatch(fetchPopularMovies(1));
+  }, [dispatch]);
 
-  const handlePageChange = (
-    event: React.ChangeEvent<unknown>,
-    value: number
-  ) => {
-    setPage(value);
-  };
-  console.log(movies);
+  const isLoaded = status === "succeeded";
 
   return (
     <div>
       <EmptyLayout>
-        <Container sx={{ py: 0, mt: 8 }}>
-          <HeroBanner movie={bannerMovie} />
-          <Typography variant="h3" component="h2" gutterBottom mt={4}>
-            Popular Movies
-          </Typography>
-          <MovieCardList movies={movies} />
-          <Box display="flex" justifyContent="center" mt={4}>
-            <Pagination
-              count={totalPages}
-              page={page}
-              onChange={handlePageChange}
-              showFirstButton
-              showLastButton
-              boundaryCount={2}
-              siblingCount={2}
-              shape="rounded"
-              variant="outlined"
-              size="large"
-              sx={{
-                "& .MuiPaginationItem-root": {
-                  color: "#fff",
-                },
-                "& .MuiPaginationItem-root.Mui-selected": {
-                  backgroundColor: "rgba(64,11,7,1)",
-                  borderColor: "red",
-                },
-                "& button:hover": {
-                  borderColor: "red",
-                },
-              }}
-            />
-          </Box>
+        <Container>
+          {isLoaded && movies ? (
+            <HeroBanner movie={movies[0]} />
+          ) : (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              sx={{ height: 600, color: "white" }}
+            >
+              <Skeleton
+                variant="rectangular"
+                animation="pulse"
+                height={600}
+                sx={{
+                  backgroundColor: "rgba(255,255,255,0.10)",
+                  width: { xs: 250, sm: 350, md: 500, lg: 1000 },
+                }}
+              />
+            </Grid>
+          )}
+          <MovieCardListContainer />
         </Container>
       </EmptyLayout>
     </div>
