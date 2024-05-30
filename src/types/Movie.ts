5bec0c8bc93b7eb4041e2e9a63aadcb00341fffb
Cesar Perez <ing.cesarperz@gmail.com>
2024-05-29 22:10:31 -0700

    feat: add new movie type for api response

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -6,3 +6,10 @@ export interface Movie {
   vote_average: number;
   genre_ids: number[];
 }
+
+export interface PopularMoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
