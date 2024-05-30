7f2284fb0a4a5502cbf6172a27e507390ab84fb1
Cesar Perez <ing.cesarperz@gmail.com>
2024-05-29 22:27:40 -0700

    feat: add pagination in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, FC } from "react";
-import { Container, Typography } from "@mui/material";
+import { Container, Box, Typography, Pagination } from "@mui/material";
 import EmptyLayout from "../components/layout/Empty";
 import HeroBanner from "../components/molecules/HeroBanner";
 import MovieCardList from "../components/molecules/MovieCardList";
@@ -16,15 +16,26 @@ const Home: FC = () => {
     vote_average: 0,
     genre_ids: [],
   });
+  const [page, setPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1);
 
   useEffect(() => {
-    getPopularMovies().then((data) => {
+    getPopularMovies(page).then((data) => {
       if (data) {
-        setMovies(data);
-        setBannerMovie(data[0]);
+        setMovies(data.results);
+        setBannerMovie(data.results[0]);
+        //NOTE: should use data.total_pages but TMBD said that petition only works with pages from 1 to 500
+        setTotalPages(500);
       }
     });
-  }, []);
+  }, [page]);
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
   console.log(movies);
 
   return (
@@ -36,6 +47,32 @@ const Home: FC = () => {
             Popular Movies
           </Typography>
           <MovieCardList movies={movies} />
+          <Box display="flex" justifyContent="center" mt={4}>
+            <Pagination
+              count={totalPages}
+              page={page}
+              onChange={handlePageChange}
+              showFirstButton
+              showLastButton
+              boundaryCount={2}
+              siblingCount={2}
+              shape="rounded"
+              variant="outlined"
+              size="large"
+              sx={{
+                "& .MuiPaginationItem-root": {
+                  color: "#fff",
+                },
+                "& .MuiPaginationItem-root.Mui-selected": {
+                  backgroundColor: "rgba(64,11,7,1)",
+                  borderColor: "red",
+                },
+                "& button:hover": {
+                  borderColor: "red",
+                },
+              }}
+            />
+          </Box>
         </Container>
       </EmptyLayout>
     </div>
