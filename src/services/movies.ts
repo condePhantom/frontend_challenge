1d778c642d7db1ccc631269563bacde90a230820
Cesar Perez <ing.cesarperz@gmail.com>
2024-05-29 22:11:06 -0700

    feat: update getPopularMovies endpoint

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -1,18 +1,18 @@
 
 import { get_api } from "./api";
-import { Movie } from '../types/Movie';
+import { Movie, PopularMoviesResponse } from '../types/Movie';
 
-export const getPopularMovies = async (): Promise<Movie[]> => {
+
+export const getPopularMovies = async (page: number =1): Promise<PopularMoviesResponse | null> => {
   try {
-    const result = await get_api('movie/popular?page=1');
+    const result = await get_api(`movie/popular?page=${page}`);
     if (typeof result === 'string') {
       throw new Error('La respuesta de la API es una cadena en lugar de un objeto esperado');
     }
-    //NOTE: check this error
-    return result.results;
+    return result;
   } catch(error) {
     console.error("Error fetching data", error);
-    return [];
+    return null;
   }
 };
 
