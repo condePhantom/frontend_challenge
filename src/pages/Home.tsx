da7ed5b75342f9121b69c41bd9cf4f6cc0d51034
Cesar Perez <ing.cesarperz@gmail.com>
2024-05-31 17:06:55 -0700

    feat: update home page to hide Hero banner in mobile and put a DetailedMovieCard component instead

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
-import React, { useEffect, FC } from "react";
-import { Container, Skeleton, Grid } from "@mui/material";
+import { useEffect, FC } from "react";
+import {
+  Container,
+  Skeleton,
+  Grid,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import EmptyLayout from "../components/layout/Empty";
 import HeroBanner from "../components/molecules/HeroBanner";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,10 +13,13 @@ import { RootState, AppDispatch } from "../store";
 import { fetchPopularMovies } from "../store/movieSlice";
 import { fetchGenresMovies } from "../store/genresSlice";
 import MovieCardListContainer from "../components/organisms/MovieCardListContainer";
+import DetailedMovieCard from "../components/atoms/DetailedMovieCard";
 
 const Home: FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const { movies, status } = useSelector((state: RootState) => state.movies);
+  const theme = useTheme();
+  const lowerMD = useMediaQuery(theme.breakpoints.down("md"));
 
   console.log(movies);
 
@@ -26,27 +35,31 @@ const Home: FC = () => {
     <div>
       <EmptyLayout>
         <Container>
-          {isLoaded && movies ? (
-            <HeroBanner movie={movies[0]} />
+          {!lowerMD ? (
+            isLoaded && movies ? (
+              <HeroBanner movie={movies[0]} />
+            ) : (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                sx={{ height: 600, color: "white" }}
+              >
+                <Skeleton
+                  variant="rectangular"
+                  animation="pulse"
+                  height={600}
+                  sx={{
+                    backgroundColor: "rgba(255,255,255,0.10)",
+                    width: { xs: 250, sm: 350, md: 500, lg: 1000 },
+                  }}
+                />
+              </Grid>
+            )
           ) : (
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              md={4}
-              lg={3}
-              sx={{ height: 600, color: "white" }}
-            >
-              <Skeleton
-                variant="rectangular"
-                animation="pulse"
-                height={600}
-                sx={{
-                  backgroundColor: "rgba(255,255,255,0.10)",
-                  width: { xs: 250, sm: 350, md: 500, lg: 1000 },
-                }}
-              />
-            </Grid>
+            movies && <DetailedMovieCard movie={movies[0]} />
           )}
           <MovieCardListContainer />
         </Container>
