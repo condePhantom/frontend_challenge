124a8589599ff118d2ac475fb6113920442d803e
Cesar Perez <ing.cesarperz@gmail.com>
2024-05-31 17:07:45 -0700

    feat: add MovieDetail to routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { FC } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-
+import MovieDetail from "./pages/MovieDetail";
 const App: FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
