b311024659db21aff4b090de791854f0b2f147a3
Cesar Perez <ing.cesarperz@gmail.com>
2024-05-31 23:29:13 -0700

    feat: document all files in types folder

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,3 +1,12 @@
+/**
+ * @interface Moview - Type for movie
+ * @property { number } id - The id of the movie
+ * @property { string } title - The title of the movie
+ * @property { string } poster_path - The path of the image of the movie
+ * @property { string } overview - The description of the movie
+ * @property { number } vote_average - The score of the movie
+ * @property { number[] } genre_ids - An array of ids of genres
+ */
 export interface Movie {
   id: number;
   title: string;
@@ -7,6 +16,13 @@ export interface Movie {
   genre_ids: number[];
 }
 
+/**
+ * @interface PopularMoviesResponse - Type for the popular movies response from API
+ * @property { number } page - The current page of the request to API
+ * @property { Movie[] } results - An array of movies from API
+ * @property { number } total_pages - The total page of the request to API
+ * @property { number } total-results - The total amount of pages in the API
+ */
 export interface PopularMoviesResponse {
   page: number;
   results: Movie[];
